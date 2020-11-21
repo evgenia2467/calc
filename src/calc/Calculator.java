b4969111d7c3d9e0c1ba74b535f0b79f149c516a
evgenia2467 <evgeniamagas813@email.com>
2020-11-21 17:31:25 +0200

    Initial comit

---
+++ src/calc/Calculator.java
@@ -0,0 +1,14 @@
+package calc;
+
+public class Calculator {
+    double addTwoNumber(double first, double second) {
+        double resuld = first + second;
+        System.out.println("%f + %f = %f" first, second));
+        return resuld;
+    }
+    public class sudtrastTwoNambers {
+        double addTwoNumber(double first, double second) {
+            double resuld = first + second;
+            System.out.println("%f + %f = %f" first, second));
+            return resuld;
+}
