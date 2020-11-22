6f7021a1bc353a50e848264276629e760e6f2d5e
evgenia2467 <evgeniamagas813@email.com>
2020-11-22 17:34:56 +0200

    draft

---
+++ src/calc/CalculatorWithAnonymousClasses.java
@@ -0,0 +1,8 @@
+package calc;
+
+public class CalculatorWithAnonymousClasses {
+    static ArithmeticOreration addition = new ArithmeticOreration() {
+        public double performOperation(double first, double second) {
+        }
+    }
+}
