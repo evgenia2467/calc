03d1d332a3c1ad8b8a91680e9528b66a89bcee36
evgenia2467 <evgeniamagas813@email.com>
2020-11-22 17:14:02 +0200

    first version

--- src/calc/Calculator.java
+++ src/calc/Calculator.java
@@ -1,14 +1,24 @@
 package calc;
 
+import static java.lang.String.format;
+
 public class Calculator {
-    double addTwoNumber(double first, double second) {
+    double addTwoNumbers(double first, double second) {
         double resuld = first + second;
-        System.out.println("%f + %f = %f" first, second));
+        System.out.println(format("%f + %f = %f", first, second, resuld));
         return resuld;
     }
-    public class sudtrastTwoNambers {
-        double addTwoNumber(double first, double second) {
+
+    double subtractTwoNumbers(double first, double second) {
             double resuld = first + second;
-            System.out.println("%f + %f = %f" first, second));
+            System.out.println(format("%f - %f = %f", first, second, resuld));
             return resuld;
 }
+
+
+        public static void main(String[] args) {
+          var calc = new Calculator();
+            calc.addTwoNumbers(2.1, 3.4);
+          calc.subtractTwoNumbers(2.1, 3.4);
+        }
+    }
